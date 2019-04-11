eded06b3dfc20a06938b85cd3764b6ebf0fca146
Zack <znadrich@gmail.com>
2019-04-10 22:29:40 -0400

    Multiplot function

--- R/functions.R
+++ R/functions.R
@@ -69,3 +69,71 @@ logOddsPlot <- function(df, char, cuts, min, filter_n){
     geom_point() + 
     xlab(char)
 }
+
+# Multiple plot function
+#
+# ggplot objects can be passed in ..., or to plotlist (as a list of ggplot objects)
+# - cols:   Number of columns in layout
+# - layout: A matrix specifying the layout. If present, 'cols' is ignored.
+#
+# If the layout is something like matrix(c(1,2,3,3), nrow=2, byrow=TRUE),
+# then plot 1 will go in the upper left, 2 will go in the upper right, and
+# 3 will go all the way across the bottom.
+#
+multiplot <- function(..., plotlist=NULL, file, cols=1, layout=NULL) {
+  # Make a list from the ... arguments and plotlist
+  plots <- c(list(...), plotlist)
+  
+  numPlots = length(plots)
+  
+  # If layout is NULL, then use 'cols' to determine layout
+  if (is.null(layout)) {
+    # Make the panel
+    # ncol: Number of columns of plots
+    # nrow: Number of rows needed, calculated from # of cols
+    layout <- matrix(seq(1, cols * ceiling(numPlots/cols)),
+                     ncol = cols, nrow = ceiling(numPlots/cols))
+  }
+  
+  if (numPlots==1) {
+    print(plots[[1]])
+    
+  } else {
+    # Set up the page
+    grid.newpage()
+    pushViewport(viewport(layout = grid.layout(nrow(layout), ncol(layout))))
+    
+    # Make each plot, in the correct location
+    for (i in 1:numPlots) {
+      # Get the i,j matrix positions of the regions that contain this subplot
+      matchidx <- as.data.frame(which(layout == i, arr.ind = TRUE))
+      
+      print(plots[[i]], vp = viewport(layout.pos.row = matchidx$row,
+                                      layout.pos.col = matchidx$col))
+    }
+  }
+}
+
+categoricalPlot <- function(df, char){
+  df$var <- df[, char]
+  
+  plot_df <- df %>%
+    group_by(var) %>%
+    mutate(donated_num = ifelse(donated == 'Yes', 1, 0)) %>%
+    summarize(n = n(),
+              donated = mean(donated_num))
+  
+  plt1 <- plot_df %>%
+    ggplot(aes(x = var, y = n)) +
+    geom_bar(stat = 'identity') + 
+    coord_flip() +
+    xlab(char)
+  
+  plt2 <- plot_df %>%
+    ggplot(aes(x = var, y = donated)) +
+    geom_bar(stat = 'identity') + 
+    coord_flip() +
+    xlab(char) 
+  
+  multiplot(plt2, plt1, cols = 1)
+}
